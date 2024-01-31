5a254da74b1b7cbf03861b0a0cbcfcbb64956cfe
Thomas <thomas.dong@vucar.net>
2024-01-31 21:21:18 +0700

    gradient landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default async function HomePage() {
   return (
-    <main className="w-full py-12 md:py-24 lg:py-32">
+    <main className="w-full py-12 md:py-24 lg:py-32 bg-gradient-to-b from-pink-100 via-pink-200 to-pink-300">
       <div className="container px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <h1 className="text-primary text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
